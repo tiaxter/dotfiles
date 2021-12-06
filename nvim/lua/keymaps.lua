25839bdf2f3069049b149c4688293764966b5e64
Gerardo Palmiotto <jerrytiapalmiotto@gmail.com>
2021-12-06 22:19:24 +0100

    Added key bindings to switch and close tab

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -10,7 +10,12 @@ keymap('n', ';f', ":lua require('telescope.builtin').find_files{}<CR>", {noremap
 keymap('n', ';g', ":lua require('telescope.builtin').git_files{}<CR>", {noremap = true})
 keymap('n', '//', ":lua require('telescope.builtin').buffers{}<CR>", {noremap = true})
 
+-- Switch between buffers
+keymap('n', '<C-i>', ':BufferNext<CR>', { noremap = true, silent = true})
+keymap('n', '<C-q>', ':BufferClose<CR>', { noremap = true, silent = true})
+
 -- Use emmet expanding using TAB key
 vim.api.nvim_command([[
   imap <expr> <tab> emmet#expandAbbrIntelligent("\<tab>")
 ]])
+
